feebe858404826e7b7d93736821c7852d8269308
tegomez <tegomez@fi.uba.ar>
2022-10-10 13:21:22 -0300

    diseño aboutUS

--- Practicar Facultad/js/aboutUs/slider.js
+++ Practicar Facultad/js/aboutUs/slider.js
@@ -1,28 +1,26 @@
-(function(){
-    const sliders=[...document.querySelectorAll('.slider_body')];
-    const arrowNext=document.querySelector('#next');
-    const arrowBefore=document.querySelector('#before');
+(function () {
+    const sliders = [...document.querySelectorAll('.slider_body')];
+    const arrowNext = document.querySelector('#next');
+    const arrowBefore = document.querySelector('#before');
     let value;
 
-    arrowNext.addEventListener('click', ()=>changePosition(1));
+    arrowNext.addEventListener('click', () => changePosition(1));
 
-    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+    arrowBefore.addEventListener('click', () => changePosition(-1));
 
-    function changePosition(change){
-        const currentElement=Number(document.querySelector('.slider_body--show').dataset.id);
+    function changePosition(change) {
+        const currentElement = Number(document.querySelector('.slider_body--show').dataset.id);
 
-        value=currentElement;
-        value+=change;
+        value = currentElement;
+        value += change;
 
-        if (value===0 || value==sliders.length+1){
-            value=value===0? sliders.length : 1;
+        if (value === 0 || value == sliders.length + 1) {
+            value = value === 0 ? sliders.length : 1;
 
         }
 
-        sliders[currentElement-1].classList.toggle('slider_body--show');
-        sliders[value-1].classList.toggle('slider_body--show');
+        sliders[currentElement - 1].classList.toggle('slider_body--show');
+        sliders[value - 1].classList.toggle('slider_body--show');
     }
-
-
 
 })()
