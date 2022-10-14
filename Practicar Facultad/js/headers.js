357a45bad6399117dd769e25e1d4cb17b81c88e4
CristianIglesias <iglesias.cristian99@gmail.com>
2022-10-14 12:56:40 -0300

    Headers Dinamicos en js

---
+++ Practicar Facultad/js/headers.js
@@ -0,0 +1,27 @@
+var Headersito = document.querySelector("#header").innerHTML=
+`<header>
+<div class="container_menu"> 
+    <a href="index.html"  class="logo">
+        <img class="logo_menu" src="img/logo_violeta.png" alt="">
+    </a>
+
+   
+    <input type="checkbox" id="menu_bar">
+    <label for="menu_bar" class="check_btn">
+        <i class="fas fa-bars"></i>
+    </label>
+   
+
+    <div class="menu">
+        <nav>
+            <ul>
+                <li><a href="/TPO_CodoaCodo_22518/Practicar Facultad/index.html" id="selected">Inicio</a></li>
+                <li><a href="/TPO_CodoaCodo_22518/Practicar Facultad/html/tutorialesv1.html">Servicios</a></li>
+                <li><a href="/TPO_CodoaCodo_22518/Practicar Facultad/html/aboutUs.html">Sobre Nosotros</a></li>
+                <li><a href="/TPO_CodoaCodo_22518/Practicar Facultad/html/contact.html">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+</header>`;
